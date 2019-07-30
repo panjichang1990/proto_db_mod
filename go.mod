ee5f8304e1d8c8262ba948a8c359637a7f385136
pjc <459740627@qq.com>
2019-07-30 16:27:55 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module proto_db_mod
+
+go 1.12
+
+require (
+	github.com/golang/protobuf v1.3.2
+	github.com/micro/go-micro v1.7.0
+)
---
+++ test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"proto_db_mod/proto_server"
+)
+
+func main()  {
+	a:=proto_server.Item{}
+	fmt.Println(a)
+
+}
